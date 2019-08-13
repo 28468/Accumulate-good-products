b6bff9c793f7e14e1f8826342f6c62411bbaef33
jk <1805914473@qq.com>
2019-08-14 07:53:13 +0800

    修改

--- 积纳有品/src/store/index.js
+++ 积纳有品/src/store/index.js
@@ -2,13 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import CreateLogger from 'vuex/dist/logger';
 // 引入store子模块
- import shouTab from './modules/shouTab.js';
+import shouTab from './modules/shouTab.js';
 import home from './modules/home';
 
 Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
-     shouTab,
+    shouTab,
     home,
   },
   plugins: [CreateLogger()]
