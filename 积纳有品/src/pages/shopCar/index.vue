04031febf2183239ec08d35ec0644b3167b649c2
jk <1805914473@qq.com>
2019-08-13 08:59:43 +0800

    排版

---
+++ 积纳有品/src/pages/shopCar/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper">
+   购物车
+    
+  </div>
+</template>
+<script>
+export default {
+  props:{
+
+  },
+  components:{
+
+  },
+  data(){
+    return {
+
+    }
+  },
+  computed:{
+
+  },
+  methods:{
+
+  },
+  created(){
+
+  },
+  mounted(){
+
+  }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
